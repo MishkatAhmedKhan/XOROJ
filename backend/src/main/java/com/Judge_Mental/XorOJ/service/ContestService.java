780ad736546579d818b393f35d529325c4c50962
sssatty <sohomsattyam@iut-dhaka.edu>
2026-01-27 11:41:00 +0600

    contest creation

--- backend/src/main/java/com/Judge_Mental/XorOJ/service/ContestService.java
+++ backend/src/main/java/com/Judge_Mental/XorOJ/service/ContestService.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.Judge_Mental.XorOJ.repo.ContestRepository;
+<<<<<<< HEAD
 import com.Judge_Mental.XorOJ.model.Contest;
+=======
+import com.Judge_Mental.XorOJ.util.Pair;
+>>>>>>> 7f03888a (contest creation)
 
 
 @Service
@@ -14,6 +18,9 @@ public class ContestService {
 
     @Autowired
     private ContestRepository contestRepo;
+    
+    @Autowired
+    private ProblemService problemService;
 
     public List<Contest> getAllContests() {
         return contestRepo.findAll();
@@ -38,4 +45,52 @@ public class ContestService {
         }
         return null;
     }
+<<<<<<< HEAD
+=======
+
+
+    //Edit
+
+    public boolean updateContestGeneralInfo(Long contestId, String title, String description, LocalDateTime startTime, LocalDateTime endTime) {
+        Contest contest = findById(contestId);
+        System.out.println("Updating contest: " + contestId);
+        if (contest != null) {
+            contest.setTitle(title);
+            contest.setDescription(description);
+            contest.setStartTime(startTime);
+            contest.setEndTime(endTime);
+            contest.setDuration((int) ChronoUnit.MINUTES.between(startTime, endTime));
+            contestRepo.save(contest);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean updateContestProblems(Long contestId, List<Pair<Long, Integer>> requests) {
+        Contest contest = findById(contestId);
+        if (contest != null) {
+            Set<Problem> newProblemSet = new HashSet<>();
+            
+            // Fetch and add each problem in the order specified by the requests list
+            for (Pair<Long, Integer> request : requests) {
+                Long problemId = request.getFirst();
+                Integer problemNum = request.getSecond();
+                
+                Problem problem = problemService.findProblemById(problemId);
+                if (problem != null) {
+                    problem.setProblemNum(problemNum);
+                    newProblemSet.add(problem);
+                }
+            }
+            
+            // Replace all problems with the new set
+            contest.getProblems().clear();
+            contest.getProblems().addAll(newProblemSet);
+            
+            contestRepo.save(contest);
+            return true;
+        }
+        return false;
+    }
+>>>>>>> 7f03888a (contest creation)
 }
